f74b55cd84e4554f9c236c8f2b5c57a13077118a
Rohit Shankar Ragmahale <rohitragmahale.mobile@gmail.com>
2024-03-15 23:22:35 +0530

    Initial Commit

---
+++ ReduxApplication/ReduxApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxApplicationApp.swift
+//  ReduxApplication
+//
+//  Created by Rohit Ragmahale on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ReduxApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
